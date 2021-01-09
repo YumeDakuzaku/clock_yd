3555277c70062c88d2199aa0ba32a1011cf508da
YumeDakuzaku <yumedakuzaku@yahoo.com.br>
2021-01-09 01:04:44 -0300

    Alteração de cor

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def time():
     label.after(1000, time)
 
 
-label = Label (root, font=("arial", 60), background = "black", foreground ="purple")
+label = Label (root, font=("arial", 60), background = "black", foreground ="cyan")
 label.pack(anchor='center')
 
 time()
