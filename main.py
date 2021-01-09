7e89583bdeb1b9ecee8b919d15fb0cd11a09866b
YumeDakuzaku <yumedakuzaku@yahoo.com.br>
2021-01-09 01:16:16 -0300

    Alteração fonte

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def time():
     label.after(1000, time)
 
 
-label = Label (root, font=("arial", 60), background = "black", foreground ="cyan")
+label = Label (root, font=("Verdana", 60), background = "black", foreground ="cyan")
 label.pack(anchor='center')
 
 time()
